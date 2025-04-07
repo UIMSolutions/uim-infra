3b43029770c42030846865c26f45a7f866368e03
UIMSolutions <ui.manufaktur@gmail.com>
2025-04-07 07:46:26 +0200

    Add ORM and validations modules with Apache 2.0 license
    - Created 'orm' directory with LICENSE, dub.sdl, and dub.selections.json files.
    - Added 'routings' directory with LICENSE, dub.sdl, and dub.selections.json files.
    - Introduced 'validations' directory with LICENSE, dub.sdl, and dub.selections.json files.
    - Implemented .gitignore and FUNDING.yml files for 'routings' and 'validations' directories.
    - Added infralevel module in 'uim' directory, importing necessary components.

---
+++ uim/infralevel.d
@@ -0,0 +1,31 @@
+/****************************************************************************************************************
+* Copyright: © 2018-2025 Ozan Nurettin Süel (aka UIManufaktur)                                                  *
+* License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.         *
+* Authors: Ozan Nurettin Süel (aka UIManufaktur)                                                                *
+*****************************************************************************************************************/
+module uim.infralevel;
+
+public {
+    import std.functional;
+    import std.conv;
+    import std.datetime;
+    import std.exception;
+}
+
+public {
+    import uim.lowlevel;
+    
+    import uim.databases;
+    import uim.datasources;
+    import uim.entitybases;
+    import uim.filesystems;
+    import uim.http;
+    import uim.jsonbases;
+    import uim.networks;
+    import uim.orm;
+    import uim.routings;
+    import uim.servers;
+    import uim.storage;
+    import uim.validations;
+    import uim.virtualization;
+}
