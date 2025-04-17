08c43752b9c980a028142e3accb3a1ab099427d0
UIMSolutions <ui.manufaktur@gmail.com>
2025-04-17 08:12:19 +0200

    Add copyright and license information to multiple D files for compliance

--- orm/uim/orm/classes/associations/associaton.d
+++ orm/uim/orm/classes/associations/associaton.d
@@ -1,10 +1,14 @@
+/****************************************************************************************************************
+* Copyright: © 2018-2025 Ozan Nurettin Süel (aka UIManufaktur)                                                  *
+* License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.         *
+* Authors: Ozan Nurettin Süel (aka UIManufaktur)                                                                *
+*****************************************************************************************************************/
 module uim.orm.classes.associations.association;
 
 mixin(Version!"test_uim_orm");
 
 import uim.orm;
 @safe:
-
 
 class DORMAssociation : UIMObject, IORMAssociation {
     mixin(ORMAssociationThis!());
